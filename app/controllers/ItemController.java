22f52db0a5d0224904e040130d8624669c9590b0
JDTawa <jd.torres11@uniandes.edu.co>
2016-08-22 08:39:34 -0500

    1/2 crud :V
    Post & get de todas las clases completado

--- app/controllers/ItemController.java
+++ app/controllers/ItemController.java
@@ -3,5 +3,38 @@ package controllers;
 /**
  * Created by jd.torres11 on 20/08/2016.
  */
-public class ItemController {
+import dispatchers.AkkaDispatcher;
+import java.util.concurrent.CompletableFuture;
+import static play.libs.Json.toJson;
+import models.ItemEntity;
+import akka.dispatch.MessageDispatcher;
+import play.mvc.*;
+import java.util.concurrent.CompletionStage;
+import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class ItemController extends Controller {
+    public CompletionStage<Result> getItems() {
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+
+        return CompletableFuture.
+                supplyAsync(() -> { return ItemEntity.FINDER.all(); } ,jdbcDispatcher)
+                .thenApply(itemEntities -> {return ok(toJson(itemEntities));}
+                );
+    }
+    public CompletionStage<Result> createItem(){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        JsonNode nItem = request().body().asJson();
+        ItemEntity item = Json.fromJson( nItem , ItemEntity.class ) ;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    item.save();
+                    return item;
+                }
+        ).thenApply(
+                ItemEntity -> {
+                    return ok(Json.toJson(ItemEntity));
+                }
+        );
+    }
 }
--- app/controllers/WishListController.java
+++ app/controllers/WishListController.java
@@ -3,5 +3,39 @@ package controllers;
 /**
  * Created by jd.torres11 on 20/08/2016.
  */
-public class WishListController {
+import models.WishListEntity;
+import play.mvc.Controller;
+import dispatchers.AkkaDispatcher;
+import java.util.concurrent.CompletableFuture;
+import static play.libs.Json.toJson;
+import akka.dispatch.MessageDispatcher;
+import play.mvc.*;
+import java.util.concurrent.CompletionStage;
+import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class WishListController extends Controller {
+    public CompletionStage<Result> getWishList() {
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+
+        return CompletableFuture.
+                supplyAsync(() -> { return WishListEntity.FINDER.all(); } ,jdbcDispatcher)
+                .thenApply(wishList -> {return ok(toJson(wishList));}
+                );
+    }
+    public CompletionStage<Result> createWishList(){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        JsonNode nItem = request().body().asJson();
+        WishListEntity wishList = Json.fromJson( nItem , WishListEntity.class ) ;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    wishList.save();
+                    return wishList;
+                }
+        ).thenApply(
+                WishListEntity -> {
+                    return ok(Json.toJson(WishListEntity));
+                }
+        );
+    }
 }
